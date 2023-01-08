2f3ab29e3123608201dfe1657d5ab1703fe08d77
vsevolodjarkov <94003560+VsevolodZharkov@users.noreply.github.com>
2023-01-08 12:50:17 +0200

    save

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const btn = form.querySelector(".form_btn");
 const input = form.querySelector(".form_input-two");
 const result = {};
 let type;
-let inputValue;
+let inputValue = '';
 document.addEventListener("keydown", (event) => {
   event.preventDefault();
   if (event.code.toLowerCase() === "space") {
@@ -34,7 +34,7 @@ document.addEventListener("click", (event) => {
     alert("Text copied");
   }
 });
-
+// SUBMIT
 document.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -48,9 +48,38 @@ document.addEventListener("submit", (e) => {
   result.column = column;
   // console.log(result);
   columnСheck(result);
+
+	setTextColor(text, color);
+  setTextColor(btn, color);
   return result;
 });
 
+input.addEventListener("keydown", (e) => {
+	// console.log(e.key || e.code);
+  if (
+    e.key.toLowerCase() === "escape" ||
+    e.code.toLowerCase() === "space" ||
+		e.code.toLowerCase() === "enter" ||
+		e.key.toLowerCase() === "shift" ||
+		e.key.toLowerCase() === "control"
+  ) {
+    return;
+  }
+	if ( e.key.toLowerCase() === "backspace" ) {
+		const a = [...inputValue].slice(0, inputValue.length - 1).join('');
+		const result = String(a);
+		inputValue = result.trim();
+		e.target.value = result;
+		return;
+	}
+  inputValue += e.key;
+  e.target.value = inputValue.trim();
+});
+
+
+// if(e.target.value.length < 7 || e.target.value.length > 7) {
+// 	alert('Hex color legth 1')
+// }
 function columnСheck({ color, column }) {
   const col = document.getElementById(`${column}`);
   const isLocked = col.querySelector("i").classList.contains("fa-lock");
@@ -58,7 +87,11 @@ function columnСheck({ color, column }) {
   if (isLocked) {
     alert("This column is disabled.");
   } else if (!isLocked) {
-    col.style.background = color;
+		if(inputValue === ''){
+			col.style.background = color;
+		} else if(inputValue !== '') {
+			col.style.background = inputValue;
+		}
   }
 }
 // function generateRandomColor() {
@@ -126,23 +159,6 @@ function getColorsFromHash() {
   return [];
 }
 
-input.addEventListener("keydown", (e) => {
-  
-  console.log(e.key.toLowerCase());
-  if (
-    e.key.toLowerCase() === "escape" ||
-    e.code.toLowerCase() === "space" ||
-		e.code.toLowerCase() === "enter"
-  ) {
-    return;
-  }
-	if ( e.key.toLowerCase() === "backspace" ) {
-		inputValue.slice(0, Number(inputValue.length - 2));
-		e.target.value = inputValue;
-		return;
-	}
-  inputValue += e.key;
-  e.target.value = inputValue;
-});
+
 
 setRandomColor(true);
